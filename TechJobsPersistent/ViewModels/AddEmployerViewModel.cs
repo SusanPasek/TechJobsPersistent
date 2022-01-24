6801e73685115ceec729d984f87b732aa6f19d4b
SusanPasek <susanpasekm@gmail.com>
2022-01-24 02:19:44 -0600

    completed A4

---
+++ TechJobsPersistent/ViewModels/AddEmployerViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddEmployerViewModel
+    {
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Name must be between 3 and 30 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Employer can only have one location")]
+        public string Location { get; set; }
+    }
+}
